10b2675e812caa8a328113062a20034e2a8348a0
michelmany <hello@michelmany.com>
2025-07-26 17:27:17 +0200

    chore(api): setup mock API with JSON Server and initial items

--- mocks/api/server.js
+++ mocks/api/server.js
@@ -1,5 +1,5 @@
-const jsonServer = require('json-server');
 const path = require('path');
+const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
